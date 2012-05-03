87b0c51273a7629592601fa3d452f1918fd85f2a
Sam Figueroa <unimatrixzxero+gravatar@gmail.com>
2012-05-03 04:08:24 +0200

    Add basic lib for teleportd api

---
+++ lib/teleportd.rb
@@ -0,0 +1,39 @@
+require 'curb'
+require 'open-uri'
+require 'hashie'
+require 'json'
+require 'awesome_print'
+
+module Teleportd
+  END_POINT = 'http://api.teleportd.com/'.freeze
+  DEFAULT_FETCH_SIZE = 100.freeze
+  class Config
+    class << self
+      attr_accessor :token
+    end
+  end
+
+  def query(method, params)
+    uri = api_path(method) + '&' + params
+    puts uri
+    results = JSON::parse Curl::Easy.perform(uri).body_str
+    results = (results && results['ok'] ? results : {})
+    Hashie::Mash.new(results)
+  end
+
+  def api_path(method)
+     "#{END_POINT}#{method}?user_key=#{Teleportd::Config.token}"
+  end
+
+  def search_location(lat, long, vertical_span = 1, horizontal_span = 1, page = 0)
+    query('search', "loc=[#{normalize_coords(lat)},#{normalize_coords(long)},#{vertical_span.to_f},#{horizontal_span.to_f}]&size=DEFAULT_FETCH_SIZE&from=#{page * 100}").hits
+  end
+
+  def details(sha)
+    query 'get', "sha=#{sha}"
+  end
+
+  def normalize_coords(coord)
+    "%2.2f" % coord
+  end
+end
